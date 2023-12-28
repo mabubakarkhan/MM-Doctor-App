5aa321abcd6843d1881905dc40cc3ebcfb498a6e
M Abu Bakar Khan <mabubakarkhan@users.noreply.github.com>
2023-12-28 20:25:58 +0500

    fixing

--- application/views/doctor/appointments.php
+++ application/views/doctor/appointments.php
@@ -23,50 +23,54 @@
 
 							<div class="col-md-7 col-lg-8 col-xl-9">
 								<div class="row">
-									<?php foreach ($appointments as $key => $q): ?>
-										<div class="col-md-12 col-sm-6 col-lg-4 ">
-											<div class="card widget-profile pat-widget-profile">
-												<div class="card-body">
-													<div class="pro-widget-content">
-														<div class="profile-info-widget">
-															<a href="patient-profile.html" class="booking-doc-img">
-																<img src="<?=UPLOADS.$q['patientImg']?>" alt="User Image">
-															</a>
-															<div class="profile-det-info">
-																<h3><a href="javascript://"><?=$q['patientFname'].' '.$q['patientLname']?> </a></h3>
-																<div class="appointment-action">
-																	<a href="#" class="btn btn-sm bg-info-light get-appointment-info" data-id="<?=$q['appointment_id']?>">
-																		<i class="feather-eye"></i>
-																	</a>
-																	<?php if ($q['status'] == 'confirm'): ?>
-																		<span class="badge rounded-pill success-status">Confirm</span>
-																	<?php elseif ($q['status'] == 'done'): ?>
-																		<span class="badge rounded-pill success-status">Done</span>
-																	<?php elseif ($q['status'] == 'pending'): ?>
-																		<span class="badge rounded-pill warning-status">Pending</span>
-																	<?php elseif ($q['status'] == 'cancel'): ?>
-																		<span class="badge rounded-pill danger-status">Cancel</span>
-																	<?php endif ?>
+									<?php if ($appointments): ?>
+										<?php foreach ($appointments as $key => $q): ?>
+											<div class="col-md-12 col-sm-6 col-lg-4 ">
+												<div class="card widget-profile pat-widget-profile">
+													<div class="card-body">
+														<div class="pro-widget-content">
+															<div class="profile-info-widget">
+																<a href="patient-profile.html" class="booking-doc-img">
+																	<img src="<?=UPLOADS.$q['patientImg']?>" alt="User Image">
+																</a>
+																<div class="profile-det-info">
+																	<h3><a href="javascript://"><?=$q['patientFname'].' '.$q['patientLname']?> </a></h3>
+																	<div class="appointment-action">
+																		<a href="#" class="btn btn-sm bg-info-light get-appointment-info" data-id="<?=$q['appointment_id']?>">
+																			<i class="feather-eye"></i>
+																		</a>
+																		<?php if ($q['status'] == 'confirm'): ?>
+																			<span class="badge rounded-pill success-status">Confirm</span>
+																		<?php elseif ($q['status'] == 'done'): ?>
+																			<span class="badge rounded-pill success-status">Done</span>
+																		<?php elseif ($q['status'] == 'pending'): ?>
+																			<span class="badge rounded-pill warning-status">Pending</span>
+																		<?php elseif ($q['status'] == 'cancel'): ?>
+																			<span class="badge rounded-pill danger-status">Cancel</span>
+																		<?php endif ?>
+																	</div>
 																</div>
 															</div>
 														</div>
-													</div>
-													<div class="patient-info">
-														<div class="patient-details">
-															<h5><i class="fas fa-user-clock"></i> <?=date('d M Y',strtotime($q['appointment_date'])).', '.date('H:i A',strtotime($q['time_start']))?></h5>
-															<?php if (isset($q['hospitalName']) && strlen($q['hospitalName']) > 0): ?>
-																<h5><i class="fas fa-map-marker-alt"></i> <?=$q['hospitalName']?></h5>
-															<?php else: ?>
-																<h5><i class="fas fa-map-marker-alt"></i> Online</h5>
-															<?php endif ?>
-															<h5><i class="fas fa-envelope"></i> <a href="mailto:<?=$q['email']?>" class="__cf_email__"><?=$q['email']?></a></h5>
-															<h5 class="mb-0"><i class="fas fa-phone"></i> <?=$q['phone']?></h5>
+														<div class="patient-info">
+															<div class="patient-details">
+																<h5><i class="fas fa-user-clock"></i> <?=date('d M Y',strtotime($q['appointment_date'])).', '.date('H:i A',strtotime($q['time_start']))?></h5>
+																<?php if (isset($q['hospitalName']) && strlen($q['hospitalName']) > 0): ?>
+																	<h5><i class="fas fa-map-marker-alt"></i> <?=$q['hospitalName']?></h5>
+																<?php else: ?>
+																	<h5><i class="fas fa-map-marker-alt"></i> Online</h5>
+																<?php endif ?>
+																<h5><i class="fas fa-envelope"></i> <a href="mailto:<?=$q['email']?>" class="__cf_email__"><?=$q['email']?></a></h5>
+																<h5 class="mb-0"><i class="fas fa-phone"></i> <?=$q['phone']?></h5>
+															</div>
 														</div>
 													</div>
 												</div>
 											</div>
-										</div>
-									<?php endforeach ?>
+										<?php endforeach ?>
+									<?php else: ?>
+										<p>no appointment found.</p>
+									<?php endif ?>
 								</div>
 							</div>
 						</div>
--- application/views/doctor/my_patients.php
+++ application/views/doctor/my_patients.php
@@ -23,35 +23,39 @@
 
 							<div class="col-md-7 col-lg-8 col-xl-9">
 								<div class="row">
-									<?php foreach ($patients as $key => $q): ?>
-										<div class="col-md-12 col-sm-6 col-lg-4">
-											<div class="card widget-profile pat-widget-profile">
-												<div class="card-body">
-													<div class="pro-widget-content">
-														<div class="profile-info-widget">
-															<a href="patient-profile.html" class="booking-doc-img">
-																<img src="<?=UPLOADS.$q['img']?>" alt="User Image">
-															</a>
-															<div class="profile-det-info">
-																<h3><a href="patient-profile.html"><?=$q['fname'].' '.$q['lname']?></a></h3>
-																<div class="patient-details">
-																	<h5 class="pat-id">Patient ID : <span><?=$q['patient_id']?></span></h5>
-																	<h5 class="mb-0"><i class="feather-map-pin"></i> <?=$q['cityName']?></h5>
+									<?php if ($patients): ?>
+										<?php foreach ($patients as $key => $q): ?>
+											<div class="col-md-12 col-sm-6 col-lg-4">
+												<div class="card widget-profile pat-widget-profile">
+													<div class="card-body">
+														<div class="pro-widget-content">
+															<div class="profile-info-widget">
+																<a href="patient-profile.html" class="booking-doc-img">
+																	<img src="<?=UPLOADS.$q['img']?>" alt="User Image">
+																</a>
+																<div class="profile-det-info">
+																	<h3><a href="patient-profile.html"><?=$q['fname'].' '.$q['lname']?></a></h3>
+																	<div class="patient-details">
+																		<h5 class="pat-id">Patient ID : <span><?=$q['patient_id']?></span></h5>
+																		<h5 class="mb-0"><i class="feather-map-pin"></i> <?=$q['cityName']?></h5>
+																	</div>
 																</div>
 															</div>
 														</div>
-													</div>
-													<div class="patient-info">
-														<ul>
-															<li>Phone <span><?=$q['phone']?></span></li>
-															<li>Gender <span><?=$q['gender']?></span></li>
-															<li>Blood Group <span><?=$q['blood_group']?></span></li>
-														</ul>
+														<div class="patient-info">
+															<ul>
+																<li>Phone <span><?=$q['phone']?></span></li>
+																<li>Gender <span><?=$q['gender']?></span></li>
+																<li>Blood Group <span><?=$q['blood_group']?></span></li>
+															</ul>
+														</div>
 													</div>
 												</div>
-											</div>
-										</div><!-- /12 -->
-									<?php endforeach ?>
+											</div><!-- /12 -->
+										<?php endforeach ?>
+									<?php else: ?>
+										<p>no patient found</p>
+									<?php endif ?>
 								</div>
 							</div>
 						</div>
